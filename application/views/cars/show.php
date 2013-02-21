5a1ca124c574d5daeaf584cb2e0b754933ddbe01
mgpyone <mgpyone@mysteryzillion.com>
2013-02-21 18:49:56 +0630

    Fixed the URL for the adding the new car controller

--- application/views/cars/show.php
+++ application/views/cars/show.php
@@ -14,7 +14,7 @@
 			</tr>
 			<tr>
 				<th align="right" scope="row">min_price</th>
-				<td><?php echo $car->min_price ?></td>	
+				<td><?php echo $car->min_price ?></td>
 			</tr>
 			<tr>
 				<th align="right" scope="row">owner_name</th>
@@ -34,7 +34,7 @@
 			</tr>
 			<tr>
 				<th align="right" scope="row">created_by</th>
-				<td><?php echo $car->created_by ?></td>	
+				<td><?php echo $car->created_by ?></td>
 			</tr>
 			<tr>
 				<th align="right" scope="row">created_at</th>
@@ -43,7 +43,7 @@
 			<tr>
 				<th align="right" scope="row">&nbsp;</th>
 				<td>
-					<a href="<?php echo base_url(); ?>/car_controller/"/><input type="submit" name="submit" value="Ok" </a></td>
+					<a href="<?php echo base_url(); ?>car_controller/"/><input type="submit" name="submit" value="Ok" </a></td>
 			</tr>
 </table>
 </body>
